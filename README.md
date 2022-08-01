fd549083c69fbae369e44cd1ffb09909a9a47b38
Sohan Basak <ronnie.basak96@gmail.com>
2022-08-01 14:12:45 -0700

    Initialised

---
+++ README.md
@@ -0,0 +1,3 @@
+PYZTEC
+
+A open source, pure python implementation of Aztec 2D barcode.
---
+++ pyztec/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class PyZtec:
+    def __init__(self):
+        self.array = np.array([])
+
+    def run(self):
+        pass
+
+    def __del__(self):
+        pass
