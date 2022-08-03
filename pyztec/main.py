c4b237f4ea439749f51d1840f71425a14c315ed4
Sohan Basak <ronnie.basak96@gmail.com>
2022-08-04 03:59:14 +0530

    markers added

--- pyztec/main.py
+++ pyztec/main.py
@@ -1,7 +1,9 @@
+from glob import glob
 from turtle import shape
 import numpy as np
 import math
 
+COMPACT_AZTEC_CODE = None
 
 def draw_square(arr, ring):
     assert arr.shape[0] == arr.shape[1] and arr.shape[0] &1 == 1, "Array must be a square of odd numbers"
@@ -17,12 +19,21 @@ def draw_square(arr, ring):
         arr[q][high] = 1
         arr[q][low] = 1
 
-COMPACT_AZTEC_CODE = np.zeros(shape=(11,11), dtype=int, order='C')
-draw_square(COMPACT_AZTEC_CODE, 0)
-draw_square(COMPACT_AZTEC_CODE, 2)
-draw_square(COMPACT_AZTEC_CODE, 4)
 
+def initialize_consts():
+    global COMPACT_AZTEC_CODE
+    _COMPACT_AZTEC_CODE = np.zeros(shape=(11,11), dtype=int, order='C')
+    draw_square(_COMPACT_AZTEC_CODE, 0)
+    draw_square(_COMPACT_AZTEC_CODE, 2)
+    draw_square(_COMPACT_AZTEC_CODE, 4)
+    
+    markers = [(0,0), (0,1), (1,0), (0,-1), (1,-1), (-2, -1)]
+    for p,q in markers:
+        _COMPACT_AZTEC_CODE[p][q] = 1
 
+    COMPACT_AZTEC_CODE = np.copy(_COMPACT_AZTEC_CODE)
+
+initialize_consts()
 # constant = 
 
 
